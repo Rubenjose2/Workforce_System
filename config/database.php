b90e7b02dde023ea6d5d38df99b2ff55d4ef9831
Ruben Castro <rubenjose@hotmail.com>
2018-02-15 15:53:24 -0500

    heroku adding database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'tuy8t6uuvh43khkk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'det1qzuc154ry5rj'),
+            'username' => env('DB_USERNAME', 'ey3x75suxnyngc6p'),
+            'password' => env('DB_PASSWORD', 'ebouo42ftr7mbkug'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
